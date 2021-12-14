56bbaca4956ec098de83e8bed936d1324fc2ee11
pieterbikkel <pieterbikkel@Pieters-MacBook-Pro.local>
2021-12-14 09:27:33 +0100

    home anitmation start

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,27 @@
 import Breadcrumb from '../../components/breadcrumb/Breadcrumb';
 import './Home.css';
+import gsap from 'gsap';
+import React, { useEffect, useRef } from "react";
+import Ring from "../../assets/images/ring.svg";
 
 function Home() {
 
+  let home: any = useRef(null)
+
+  useEffect(() => {
+    gsap.to(home, {x: 400, duration: 2})
+  })
   
     return (
       <div className="home">
 
         <Breadcrumb/>
-        
-        <h1>Home</h1>
+        <h1 className='home-text' ref={el => home = el}>Home</h1>
+        <div className='jitai-animation'>
+          <img className='ring ring1' src={Ring} alt="ring animatie"/>
+          <img className='ring ring2' src={Ring} alt="ring animatie"/>
+          <img className='ring ring3' src={Ring} alt="ring animatie"/>
+        </div>
       </div>
     );
 }
