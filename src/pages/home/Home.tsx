b625eb0bc3a106219086d0e3da49c86b36128a01
pieterbikkel <pieterbikkel@Pieters-MacBook-Pro.local>
2021-12-16 14:20:40 +0100

    chippr

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,14 +2,16 @@ import Breadcrumb from '../../components/breadcrumb/Breadcrumb';
 import './Home.css';
 import gsap from 'gsap';
 import React, { useEffect, useRef } from "react";
-import Ring from "../../assets/images/ring.svg";
+import Chippr from "../../assets/images/chippr.svg";
 
 function Home() {
 
   let home: any = useRef(null)
+  let chipprLogo: any = useRef(null)
 
   useEffect(() => {
-    gsap.to(home, {x: 400, duration: 2})
+    gsap.to(home, {x: 400, duration: 2, skewX: 45})
+    gsap.to(chipprLogo, {duration: 2, skewX: 45})
   })
   
     return (
@@ -18,9 +20,7 @@ function Home() {
         <Breadcrumb/>
         <h1 className='home-text' ref={el => home = el}>Home</h1>
         <div className='jitai-animation'>
-          <img className='ring ring1' src={Ring} alt="ring animatie"/>
-          <img className='ring ring2' src={Ring} alt="ring animatie"/>
-          <img className='ring ring3' src={Ring} alt="ring animatie"/>
+          <img className='chippr' ref={el => chipprLogo = el} src={Chippr} alt="Chippr logo"/>
         </div>
       </div>
     );
